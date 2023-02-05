6d1f0be40e46cd760b42da97784687b590478566
stanislav <xxx.stas.com@gamil.com>
2023-02-05 23:38:43 +0300

    examples 25,27

---
+++ yoga/main.js
@@ -0,0 +1,7 @@
+jQueru (document).ready(function(){
+
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+   particlesJS.load('particles-js', '../yoga/config.json', function() {
+       console.log('callback - particles.js config loaded');
+     });
+   });
