6d0a53df77e58430b495aca32d77795878e92c74
stanislav <xxx.stas.com@gamil.com>
2023-02-07 11:55:46 +0300

    examples 41 43

--- examples29/Program.cs
+++ examples29/Program.cs
@@ -2,21 +2,4 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-Console.WriteLine($"\nЗадача 29. Ряд чисел преобразует в массив");
-Console.Write("Введите ряд чисел, разделенных запятой : ");
-string? seriesOfNumbers = Console.ReadLine();
 
-seriesOfNumbers = seriesOfNumbers + ",";    // дополнительня запятая для обозначения конца строки
-
-// функция удаления пробелов из строки 
-string RemovingSpaces (string series){
-  string seriesNew = "";
-  for (int i = 0; i < series.Length; i++)
-  {
-    if (series[i] != ' ') 
-    {
-      seriesNew += series[i];
-    }
-  }
-  return seriesNew;
-}
