30c46cd44e37e0d7069ca29a650830045d637e2c
stanislav <xxx.stas.com@gamil.com>
2023-02-11 15:54:03 +0300

    54 56 58

--- examples47/Program.cs
+++ examples47/Program.cs
@@ -1,6 +1,13 @@
-﻿Console.WriteLine("Задайте количество строк двумерного массива:");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+Console.WriteLine("Задайте количество строк :");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Задайте количество столбцов двумерного массива:");
+Console.WriteLine("Задайте количество столбцов :");
 int n = Convert.ToInt32(Console.ReadLine());
 double[,] twoDimArray = new double[m, n];
 Random rnd = new Random();
