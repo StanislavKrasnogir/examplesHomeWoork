eff6fd85cb6973cd1dafae57bf6d500759f577e7
stanislav <xxx.stas.com@gamil.com>
2023-02-11 15:39:17 +0300

    examples 34 34 36 41 43 47 50

---
+++ examples38/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Lab4_1 {
+    public static void main(String[] args) {
+        int[] array = {2, 5, 7, 8, 3, 0};
+    }
+
+    public static int range(int[] array, int index, int min, int max) {
+        if (index == array.length) {
+            if (index == 0)
+                return 0;
+            else
+                return max - min;
+        } else {
+            int value = array[index];
+            return range(array, index + 1, Math.min(value, min), Math.max(value, max));
+        }
+    }
+
+    public static int range(int[] array) {
+        return range(array, 0, Integer.MAX_VALUE, Integer.MIN_VALUE);
+    }
+}
